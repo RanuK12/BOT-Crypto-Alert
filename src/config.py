ad7a32524710a387e53447ce8a5a3210dee9ccdb
Emilio Ranucoli <emilio@ranuk.dev>
2024-07-25 20:35:21 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
-WHATSAPP_FROM = os.getenv('WHATSAPP_FROM')
-WHATSAPP_TO = os.getenv('WHATSAPP_TO')
-COINGECKO_API_URL = os.getenv('COINGECKO_API_URL')
+COINGECKO_API_URL = "https://api.coingecko.com/api/v3"
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+WHATSAPP_FROM = "whatsapp:+14155238886"  # Número de Twilio
+WHATSAPP_TO = os.getenv("WHATSAPP_TO")   # Número de destino
 
