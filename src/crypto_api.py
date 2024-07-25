d5b3e4e7f1ccca572eed95b7645f7b8a3a55789b
RanuK12 <90660379+RanuK12@users.noreply.github.com>
2024-07-25 20:35:40 +0200

    Update crypto_api.py

--- src/crypto_api.py
+++ src/crypto_api.py
@@ -1,13 +1,12 @@
 import requests
 from config import COINGECKO_API_URL
 
-def get_crypto_price(crypto_id):
-    response = requests.get(f'{COINGECKO_API_URL}/simple/price', params={
+def get_crypto_price(crypto_id='bitcoin'):
+    url = f"{COINGECKO_API_URL}/simple/price"
+    params = {
         'ids': crypto_id,
         'vs_currencies': 'usd'
-    })
+    }
+    response = requests.get(url, params=params)
     data = response.json()
     return data[crypto_id]['usd']
-
-if __name__ == "__main__":
-    print(get_crypto_price('bitcoin'))
